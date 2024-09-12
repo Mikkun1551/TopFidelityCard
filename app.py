f9ae84dbbb8090224330600fcff15805a334b307
Mikkun1551 <michelcalvaresi1551@gmail.com>
2024-09-12 17:36:06 +0200

    Aggiornato il db placeholder e migliorate alcune request

--- app.py
+++ app.py
@@ -1,48 +1,32 @@
 from flask import Flask, request
+from flask_smorest import abort
+from db import aziende, tipi_azienda, give_id
 
 # Creazione di una flask app
 app = Flask(__name__)
 
-# PLACEHOLDER DATABASE
-aziende = [
-    {
-        'idAzienda': 1,
-        'Nome': 'Apple',
-        'Regione': 'Lazio',
-        'Città': 'Roma',
-        'idTipoAzienda': 1,
-        'eliminato': False
-    },
-    {
-        'idAzienda': 2,
-        'Nome': 'Sony',
-        'Regione': 'Lazio',
-        'Città': 'Roma',
-        'idTipoAzienda': 1,
-        'eliminato': False
-    }
-]
 
-# PLACEHOLDER REQUEST
-@app.get('/azienda')
-def get_aziende():
-    return {'aziende': aziende}
-
-
+# REQUEST AZIENDA
 @app.post('/azienda')
-def create_azienda():
+def post_azienda():
     # Crea una nuova azienda
     request_data = request.get_json()
-    new_azienda = {
-        'idAzienda': request_data['idAzienda'],
-        'Nome': request_data['Nome'],
-        'Regione': request_data['Regione'],
-        'Città': request_data['Città'],
-        'idTipoAzienda': request_data['idTipoAzienda'],
-        'eliminato': False
-    }
-    aziende.append(new_azienda)
-    return new_azienda, 201
+    try:
+        for t_azienda in tipi_azienda:
+            if t_azienda['idTipoAzienda'] == request_data['idTipoAzienda']:
+                new_azienda = {
+                    'idAzienda': give_id('azienda'),
+                    'Nome': request_data['Nome'],
+                    'Regione': request_data['Regione'],
+                    'Città': request_data['Città'],
+                    'idTipoAzienda': request_data['idTipoAzienda'],
+                    'eliminato': False
+                }
+                aziende.append(new_azienda)
+                return {'message': "Azienda creata con successo"}, 201
+        return {'message': "Richiesta non valida"}, 400
+    except:
+        return {'message': "Richiesta non valida"}, 400
 
 
 @app.get('/azienda/<int:idAzienda>')
@@ -50,5 +34,31 @@ def get_azienda(idAzienda):
     # Ottiene i dettagli di un'azienda specifica
     for azienda in aziende:
         if azienda['idAzienda'] == idAzienda:
+            # Dettagli dell'azienda
             return azienda, 200
     return {'message': 'Azienda non trovata'}, 404
+
+
+
+# REQUEST TIPO AZIENDA
+@app.post('/tipo-azienda')
+def post_tipo_azienda():
+    # Crea un nuovo tipo di azienda
+    request_data = request.get_json()
+    try:
+        new_tipo_azienda = {
+            'idTipoAzienda': give_id('t_azienda'),
+            'Nome': request_data['Nome'],
+            'Descrizione': request_data['Descrizione']
+        }
+        tipi_azienda.append(new_tipo_azienda)
+        return {'message': "Tipo di azienda creato con successo"}, 201
+    except:
+        return {'message': "Richiesta non valida"}, 400
+
+
+@app.get('/tipo-azienda')
+def get_tipo_aziende():
+    # Ottiene tutti i tipi di azienda
+    # Elenco di tutti i tipi di azienda
+    return tipi_azienda, 200
---
+++ db.py
@@ -0,0 +1,23 @@
+# PLACEHOLDER DATABASE
+id_aziende = 0
+id_tipo_aziende = 0
+
+
+def give_id(type_op):
+    """
+    Funzione per assegnare id
+    :param type_op: Stringa che specifica quale id serve
+    :return: Restituisce l'id autoincrementato specifico
+    """
+    if type_op == 'azienda':
+        global id_aziende
+        id_aziende += 1
+        return id_aziende
+    elif type_op == 't_azienda':
+        global id_tipo_aziende
+        id_tipo_aziende += 1
+        return id_tipo_aziende
+
+
+aziende = []
+tipi_azienda = []
