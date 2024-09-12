96f7c90861031dfb8a68e5a82fc63ef9e40c3908
Mikkun1551 <michelcalvaresi1551@gmail.com>
2024-09-12 18:00:20 +0200

    Migliorie controllo delle request

--- app.py
+++ app.py
@@ -11,22 +11,34 @@ app = Flask(__name__)
 def post_azienda():
     # Crea una nuova azienda
     request_data = request.get_json()
-    try:
-        for t_azienda in tipi_azienda:
-            if t_azienda['idTipoAzienda'] == request_data['idTipoAzienda']:
-                new_azienda = {
-                    'idAzienda': give_id('azienda'),
-                    'Nome': request_data['Nome'],
-                    'Regione': request_data['Regione'],
-                    'Città': request_data['Città'],
-                    'idTipoAzienda': request_data['idTipoAzienda'],
-                    'eliminato': False
-                }
-                aziende.append(new_azienda)
-                return {'message': "Azienda creata con successo"}, 201
-        return {'message': "Richiesta non valida"}, 400
-    except:
-        return {'message': "Richiesta non valida"}, 400
+    if (
+        'Nome' not in request_data
+        or 'Regione' not in request_data
+        or 'Città' not in request_data
+        or 'idTipoAzienda' not in request_data
+    ):
+        abort(400, message="Richiesta non valida")
+    for azienda in aziende.values():
+        if (
+            request_data['Nome'] == azienda['Nome']
+            and request_data['Regione'] == azienda['Regione']
+            and request_data['Città'] == azienda['Città']
+            and request_data['idTipoAzienda'] == azienda['idTipoAzienda']
+        ):
+            abort(400, message="Richiesta non valida")
+    for t_azienda in tipi_azienda:
+        if t_azienda['idTipoAzienda'] == request_data['idTipoAzienda']:
+            new_azienda = {
+                'idAzienda': give_id('azienda'),
+                'Nome': request_data['Nome'],
+                'Regione': request_data['Regione'],
+                'Città': request_data['Città'],
+                'idTipoAzienda': request_data['idTipoAzienda'],
+                'eliminato': False
+            }
+            aziende.append(new_azienda)
+            return {'message': "Azienda creata con successo"}, 201
+    abort(400, message="Richiesta non valida")
 
 
 @app.get('/azienda/<int:idAzienda>')
@@ -36,7 +48,7 @@ def get_azienda(idAzienda):
         if azienda['idAzienda'] == idAzienda:
             # Dettagli dell'azienda
             return azienda, 200
-    return {'message': 'Azienda non trovata'}, 404
+    abort(404, message="Azienda non trovata")
 
 
 
@@ -45,6 +57,11 @@ def get_azienda(idAzienda):
 def post_tipo_azienda():
     # Crea un nuovo tipo di azienda
     request_data = request.get_json()
+    if (
+        'Nome' not in request_data
+        or 'Descrizione' not in request_data
+    ):
+        abort(400, message="Richiesta non valida")
     try:
         new_tipo_azienda = {
             'idTipoAzienda': give_id('t_azienda'),
@@ -54,7 +71,7 @@ def post_tipo_azienda():
         tipi_azienda.append(new_tipo_azienda)
         return {'message': "Tipo di azienda creato con successo"}, 201
     except:
-        return {'message': "Richiesta non valida"}, 400
+        abort(400, message="Richiesta non valida")
 
 
 @app.get('/tipo-azienda')
