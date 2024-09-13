b95372873d9e7cc8d792b23087568e39c7e66eaa
Mikkun1551 <michelcalvaresi1551@gmail.com>
2024-09-13 14:56:07 +0200

    Finita nuova configurazione dell'app per i blueprint

--- app.py
+++ app.py
@@ -1,52 +1,30 @@
-from flask import Flask, request
-from flask_smorest import abort
-from db import aziende, tipi_azienda, give_id
+from flask import Flask
+from flask_smorest import Api
+from resources.azienda import blp as AziendaBlueprint
+from resources.tipoAzienda import blp as TipoAziendaBlueprint
 
 # Creazione di una flask app
 app = Flask(__name__)
 
+# Propagare le eccezioni delle estensioni di flask al main app per leggerle
+app.config['PROPAGATE_EXCEPTIONS'] = True
+# Il titolo dell'api
+app.config['API_TITLE'] = 'TopFidelityCard'
+# Versione corrente dell'api
+app.config['API_VERSION'] = 'v1'
+# Versione di openapi, uno standard per la documentazione api
+app.config['OPENAPI_VERSION'] = '3.0.3'
+# La posizione della root dell'api, visto che tutte le request
+# iniziano con / la root è quella
+app.config['OPENAPI_URL_PREFIX'] = '/'
+# Settare il path per la documentazione dell'api tramite swagger
+app.config['OPENAPI_SWAGGER_UI_PATH'] = '/swagger-ui'
+# Settare l'url da dove prendere la documentazione dell'api
+app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
 
-# @app.post('/azienda')
-# def post_azienda():
+# Connessione tra flask smorest alla flask app
+api = Api(app)
 
-
-
-# @app.get('/azienda/<int:idAzienda>')
-# def get_azienda(idAzienda):
-
-
-
-# @app.put('/azienda/<int:idAzienda>')
-# def put_azienda(idAzienda):
-
-
-
-# @app.put('/azienda/<int:idAzienda>/delete')
-# def update_azienda_delete(idAzienda):
-
-
-
-
-
-# @app.post('/tipo-azienda')
-# def post_tipo_azienda():
-
-
-
-# @app.get('/tipo-azienda')
-# def get_tipo_aziende():
-
-
-
-# @app.get('/tipo-azienda/<int:idTipoAzienda>')
-# def get_tipo_azienda(idTipoAzienda):
-
-
-
-# @app.put('/tipo-azienda/<int:idTipoAzienda>')
-# def put_tipo_azienda(idTipoAzienda):
-
-
-
-# @app.delete('/tipo-azienda/<int:idTipoAzienda>')
-# def delete_tipo_azienda(idTipoAzienda):
+# Import dei blueprint da resources
+api.register_blueprint(AziendaBlueprint)
+api.register_blueprint(TipoAziendaBlueprint)
